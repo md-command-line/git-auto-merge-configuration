92c3a5e927ed905e29756e175ae42b2ce937440a
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-02-09 07:56:57 -0500

    create into hidden config folder. with no errors 😃

--- write_all_branches_to_file_of_current_branch_name.sh
+++ write_all_branches_to_file_of_current_branch_name.sh
@@ -1,3 +1,11 @@
 # for each branch...
 
-currbranch=`git branch | grep \* | cut -c 3-`; rm $currbranch; for BRANCH in `git branch --list | cut -c 3-`; do echo $BRANCH >> "$currbranch.txt"; echo ;  done;
+currbranch=`git branch | grep \* | cut -c 3-`; 
+if [ ! -d $automerge_config ]; then
+  mkdir .automerge_config;
+fi
+if [ -f .automerge_config/$currbranch.txt ]; then
+  rm .automerge_config/$currbranch.txt; 
+fi
+
+for BRANCH in `git branch --list | cut -c 3-`; do echo $BRANCH >> ".automerge_config/$currbranch.txt"; echo ;  done;
